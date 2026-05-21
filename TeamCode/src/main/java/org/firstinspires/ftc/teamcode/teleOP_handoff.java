be17438176803e1185ad8a1eab6762de5ab45429
bananaman-jpg <tom616bardon@gmail.com>
2026-05-21 14:29:52 +1000

    Handoff
    1 Added file: teleOP_handoff.java
    Script is now for testing on robot.
    Code adds driver handoff function with rumble confirmation.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOP_handoff.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative OpMode")
+public class teleOP_handoff extends OpMode {
+
+    public DcMotor frontRight, frontLeft, backRight, backLeft;
+    private boolean isGamepad1Driving = true;
+    private boolean previousA1 = false;
+    private boolean previousA2 = false;
+
+    @Override
+    public void init() {
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+
+
+        if(gamepad1.a && !previousA1) {
+            if (isGamepad1Driving) {
+                gamepad2.rumbleBlips(2);
+                gamepad1.rumble(500);
+            }
+            isGamepad1Driving = true;
+        }
+
+        if(gamepad2.a && !previousA2) {
+            if (isGamepad1Driving) {
+                gamepad2.rumbleBlips(2);
+                gamepad1.rumble(500);
+            }
+            isGamepad1Driving = false;
+        }
+
+        previousA1 = gamepad1.a;
+        previousA2 = gamepad2.a;
+
+        Gamepad activeGamepad = isGamepad1Driving ? gamepad1 : gamepad2;
+
+
+
+
+
+        double y = -activeGamepad.left_stick_y; // Remember, Y stick value is reversed
+        double x = activeGamepad.left_stick_x;
+        double rx = activeGamepad.right_stick_x;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
+        backRight.setPower(backRightPower);
+
+
+    }
+}
