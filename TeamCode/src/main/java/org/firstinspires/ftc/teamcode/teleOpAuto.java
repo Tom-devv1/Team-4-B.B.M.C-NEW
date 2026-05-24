bb23f7cb0603a1f5b392a1b9d0919697e8bde492
bananaman-jpg <tom616bardon@gmail.com>
2026-05-24 14:37:53 +1000

    Autonomous
    Auto fixed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpAuto.java
@@ -1,19 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name="Basic: Iterative OpMode", group="Iterative OpMode")
-public class teleOpAuto extends OpMode {
+public class teleOpAuto extends LinearOpMode {
 
     public DcMotor frontRight, frontLeft, backRight, backLeft;
 
-
     @Override
-    public void init() {
+    public void runOpMode() {
+
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
@@ -22,40 +21,44 @@ public class teleOpAuto extends OpMode {
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
-    }
+        waitForStart();
 
-    @Override
-    public void loop(){
         forward();
-        try { Thread.sleep(1000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
+        sleep(1000);
+
         turnLeft();
-        try { Thread.sleep(1000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
+        sleep(1000);
 
     }
+
     public void stopRobot() {
         frontLeft.setPower(0);
         backLeft.setPower(0);
         frontRight.setPower(0);
         backRight.setPower(0);
     }
+
     public void forward() {
         frontLeft.setPower(1);
         backLeft.setPower(1);
-        frontRight.setPower(-1);
-        backRight.setPower(-1);
+        frontRight.setPower(1);
+        backRight.setPower(1);
     }
+
     public void backward() {
         frontLeft.setPower(-1);
         backLeft.setPower(-1);
         frontRight.setPower(-1);
         backRight.setPower(-1);
     }
+
     public void turnLeft() {
         frontLeft.setPower(-1.0);
         backLeft.setPower(-1.0);
         frontRight.setPower(1.0);
         backRight.setPower(1.0);
     }
+
     public void turnRight() {
         frontLeft.setPower(1.0);
         backLeft.setPower(1.0);
@@ -63,4 +66,3 @@ public class teleOpAuto extends OpMode {
         backRight.setPower(-1.0);
     }
 }
-
